976f1b90d3d2456ea0622738ba64c57cbc3ec5a0
adalawson96 <adalawson96@gmail.com>
2023-06-24 15:33:43 +0200

    add target=_blank on icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,10 +12,10 @@ export default function Footer(){
                         </div>
                         <h2> I enjoy building relationships with individuals who want to collaborate. Let's Connect!</h2>
                         <div className="social-icons">
-                            <a href="https://github.com/adalawson96"><img src="./github.png" alt="github" /></a>
-                            <a href="mailto:adalawson96@gmail.com"><img src="./mail.png" alt="email" /></a>
-                            <a href="https://www.linkedin.com/in/ada-lawson/"><img src="./linkedin.png" alt="linkedin" /></a>
-                            <a href="https://drive.google.com/file/d/1t2nlh3drCRTf-FeRBFCvqT9dRGtUAV2T/view?usp=sharing"><img src="./resume.png" alt="resume" /></a>
+                            <a href="https://github.com/adalawson96" target="_blank"><img src="./github.png" alt="github" /></a>
+                            <a href="mailto:adalawson96@gmail.com" target="_blank"><img src="./mail.png" alt="email" /></a>
+                            <a href="https://www.linkedin.com/in/ada-lawson/" target="_blank"><img src="./linkedin.png" alt="linkedin" /></a>
+                            <a href="https://drive.google.com/file/d/1t2nlh3drCRTf-FeRBFCvqT9dRGtUAV2T/view?usp=sharing" target="_blank"><img src="./resume.png" alt="resume" /></a>
                         </div>
                     </div>
                     <div className="col-2">
