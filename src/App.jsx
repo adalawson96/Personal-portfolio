9946244b81f73ea330fcc1e0090cd0ecec256b6c
adalawson96 <adalawson96@gmail.com>
2023-06-26 00:07:54 +0200

    create form install chakra

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import Hero from './components/Hero/Hero';
 import Skill from './components/Skill/Skill';
 import Footer from './components/Footer/Footer';
 import Portfolio from './components/Portfolio/Portfolio';
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App = () => {
   return <div className={`${css.container}`}>
-    <Header />
-    <Hero />
-    <Skill />
-    <Portfolio />
-    <Footer />
+    <ChakraProvider>
+      <Header />
+      <Hero />
+      <Skill />
+      <Portfolio />
+      <Footer />
+    </ChakraProvider>
   </div>
 };
 
 export default App;
+
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 import './Footer.css';
+import { useState } from 'react';
+import {
+    Container,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Textarea,
+    FormErrorMessage,
+  } from '@chakra-ui/react';
+
+const initValues = {name:"", email:"", subject:"", message:""};
+const initState = { values: initValues};
 
 export default function Footer(){
+    const [state, setState] = useState(initState);
+    const [touched, setTouched] = useState({});
+
+    const{values} = state;
+
+    const onBlur = ({target}) => setTouched((prev) => ({...prev,
+        [target.name]:true
+    }))
+
+    const handleChange = ({ target }) => setState((prev) => ({
+        ...prev,
+        values: {
+            ...prev.values,
+            [target.name]: target.value,
+        },
+    }));
+
     return(
         <footer>
                 <div className="footer">
@@ -18,11 +48,77 @@ export default function Footer(){
                             <a href="https://drive.google.com/file/d/1t2nlh3drCRTf-FeRBFCvqT9dRGtUAV2T/view?usp=sharing" target="_blank"><img src="./resume.png" alt="resume" /></a>
                         </div>
                     </div>
-                    <div className="col-2">
-                        <h3>Send me a message:</h3><br/>
-                            <form className="row g-3">
+                </div>
+                    
+            {/* FORM */}
+            <Container maxW="450px" mt={12}>
+                <Heading>Send me a message:</Heading>
+                <FormControl isRequired isInvalid={touched.name && !values.name} mb={6}>
+                    <FormLabel>NAME</FormLabel>
+                    <Input 
+                        type="text" 
+                        name="name" 
+                        errorBorderColor="red.300"
+                        value={values.name} 
+                        onChange={handleChange}
+                        onBlur={onBlur}
+                    />
+                    <FormErrorMessage>Required</FormErrorMessage>
+                </FormControl>
+                
+                <FormControl isRequired isInvalid={touched.email && !values.name} mb={6}>
+                    <FormLabel>EMAIL</FormLabel>
+                    <Input 
+                        type="email" 
+                        name="email" 
+                        errorBorderColor="red.300"
+                        value={values.email} 
+                        onChange={handleChange}
+                        onBlur={onBlur}
+                    />
+                    <FormErrorMessage>Required</FormErrorMessage>
+                </FormControl>
+
+                <FormControl isRequired isInvalid={touched.subject && !values.name} mb={6}>
+                    <FormLabel>SUBJECT</FormLabel>
+                    <Input 
+                        type="text" 
+                        name="subject" 
+                        errorBorderColor="red.300"
+                        value={values.subject} 
+                        onChange={handleChange}
+                        onBlur={onBlur}
+                    />
+                    <FormErrorMessage>Required</FormErrorMessage>
+                </FormControl>
+                
+                <FormControl isRequired isInvalid={touched.message && !values.message}>
+                    <FormLabel>MESSAGE</FormLabel>
+                    <Textarea
+                        type="text" 
+                        name="message" 
+                        rows={5}
+                        value={values.message} 
+                        onChange={handleChange}
+                        onBlur={onBlur}
+                    />
+                    <FormErrorMessage>Required</FormErrorMessage>
+                </FormControl>
+            </Container>
+            <button
+                variant="outline"
+                coloScheme='purple'
+                disabled={!values.name || !values.email || !values.subject || !values.message}
+                onClick={onsubmit}
+            >SUBMIT</button>
+        </footer>
+    );
+}
+                    {/* <div className="col-2">
+                        <h3>Send me a message:</h3><br/> */}
+                            {/* <form className="row g-3">
                                 <div className="col-md-6">
-                                    <label htmlFor="inputEmail4" className="form-label"></label>
+                                    <label htmlFor="email" className="form-label"></label>
                                     <input type="email" className="form-control" id="inputEmail4" placeholder="EMAIL"/>
                                 </div>
                                 <div className="col-12">
@@ -36,9 +132,6 @@ export default function Footer(){
                                 <div className="col-12">
                                     <button type="submit" className="btn btn-primary">SUBMIT</button>
                                 </div>
-                            </form>
-                    </div>
-                </div>
-        </footer>
-    )
-}
+                            </form> */}
+                    {/* </div>
+                </div> */}
